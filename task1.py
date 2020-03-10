35980d997272e04d091f905138b814652058fc96
asein-maker <samiraasein@gmail.com>
2020-03-10 12:30:35 +0600

    add task

---
+++ task1.py
@@ -0,0 +1,18 @@
+#num = int(input('Enter number:'))
+#def is_even(num):
+#    if num % 2 == 0:
+#       return True
+#   else:
+#        return False
+#DO NOT remove lines below here, this is designed to test your code.
+#DO NOT remove lines below here, this is designed to test your code.
+#def test_is_even():
+#    assert is_even(2) == True
+#    assert is_even(3) == False
+#    assert is_even(8) == True
+#    assert is_even(100) == True
+#    assert is_even(101) == False
+#    print("YOUR CODE IS CORRECT!")
+#test your code by un-commenting the line(s) below
+#test_is_even()
+
